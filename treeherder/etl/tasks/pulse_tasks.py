53f96aa79b66643a8eb7ebb1ad135bca90955239
Armen Zambrano G <armenzg@mozilla.com>
2019-06-06 10:24:32 -0400

    Bug 1395254 - Consume Taskcluster Pulse messages from standard queue exchanges
    Currently, Treeherder consumes Pulse messages from an intermediary service called `taskcluster-treeherder`.
    Such service needs to be shut down and its functionality imported into Treeherder.
    
    In order to do this we need to switch to the standard Taskcluster exchanges as defined in here:
    https://docs.taskcluster.net/docs/reference/platform/queue/exchanges
    
    On a first pass we are only including the code from `taskcluster-treeherder` without changing
    much of Treeherder's code. The code is translated from Javascript to Python and only some minor
    code changes were done to reduce the difficulty on porting the code without introducing bugs.
    Internally, on this first pass, we will still have an intermediary data structure representing
    what `taskcluster-treeherder` is emitting, however, we will stop consuming the messages
    from it and be able to shut it down.
    
    Instead of consuming from one single exchange we will be consuming multiple ones. Each one representing
    a different kind of task (e.g. pending vs running).
    
    Following this work would be to get rid of the intermediary job representation which will
    clean up some of the code and some of the old tests.
    
    This change also introduces an ETL management command that can be executed like this:
    
    ```bash
    ./manage.py update_pulse_test_fixtures
    ```
    
    This command will read 100 Taskcluster Pulse messages, process them and store them as test fixtures
    under these two files: `tests/sample_data/pulse_consumer/taskcluster_{jobs,metadata}.json`
    
    In order to test this change you need to open 4 terminal windows and follow these steps:
    
    * On the first two windows `export PULSE_URL="amqp://foo:bar@pulse.mozilla.org:5671/?ssl=1"` and run the following commands:
      * `docker-compose run -e PULSE_URL backend ./manage.py pulse_listener_jobs`
      * `docker-compose run -e PULSE_URL backend ./manage.py pulse_listener_pushes`
    * On the third window run `docker-compose run backend celery -A treeherder worker -B --concurrency 5`
    * On the last window run `docker-compose up`
    * Open on your browser `http://localhost:5000`
    
    This is all explained in https://treeherder.readthedocs.io/pulseload.html.
    
    Other changes:
    
    * Import `taskcluster-treeherder` validation to ensure we're not fed garbage.
    * Change `yaml.load(f)` for `yaml.load(f, Loader=yaml.FullLoader)`. Read [this](https://github.com/yaml/pyyaml/wiki/PyYAML-yaml.load(input)-Deprecation) for details
    * Introduce `taskcluster` and `taskcluster-urls` as dependencies
    * `test_retry_missing_revision_never_succeeds` makes no sense because
    we make Json validation on the Pulse message

--- treeherder/etl/tasks/pulse_tasks.py
+++ treeherder/etl/tasks/pulse_tasks.py
@@ -5,6 +5,7 @@ import newrelic.agent
 
 from treeherder.etl.job_loader import JobLoader
 from treeherder.etl.push_loader import PushLoader
+from treeherder.etl.taskcluster_pulse.handler import handleMessage
 from treeherder.workers.task import retryable_task
 
 
@@ -15,8 +16,13 @@ def store_pulse_jobs(pulse_job, exchange, routing_key):
     """
     newrelic.agent.add_custom_parameter("exchange", exchange)
     newrelic.agent.add_custom_parameter("routing_key", routing_key)
-
-    JobLoader().process_job(pulse_job)
+    # handleMessage expects messages in this format
+    tc_th_message = handleMessage({
+        "exchange": exchange,
+        "payload": pulse_job,
+    })
+    if tc_th_message:
+        JobLoader().process_job(tc_th_message)
 
 
 @retryable_task(name='store-pulse-pushes', max_retries=10)
